68515c6369f86423a1451c8e570e54f397e45740
Reon-21 <reonleeyx@gmail.com>
2021-04-22 12:53:44 +0800

    week1 testing

---
+++ app/src/main/java/com/example/myapplication/Week1a.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+public class Week1a {
+
+    public static void main(String[] args) {
+        String name = "John";
+        double lengthofTable = 1.6;
+
+        System.out.println("Hello World!");
+        System.out.println("My name is " + name) ;
+        System.out.println("The length of this table is "+ lengthofTable + "m");
+    }
+}
